6d5bd330d54249531327a4226c8df50f6b6d2fe2
Phương Linh <23010562@st.phenikaa-uni.edu.vn>
2025-10-27 23:01:28 +0700

    .

--- calories/lib/man_hinh_chinh.dart
+++ calories/lib/man_hinh_chinh.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class man_hinh_chinh
-    extends StatefulWidget {
+class man_hinh_chinh extends StatefulWidget {
   const man_hinh_chinh({super.key});
 
   @override
@@ -9,8 +8,15 @@ class man_hinh_chinh
       _man_hinh_chinhState();
 }
 
-class _man_hinh_chinhState
-    extends State<man_hinh_chinh> {
+class _man_hinh_chinhState extends State<man_hinh_chinh> {
+  final List<Map<String,dynamic>> _meals = [
+    {'name' : 'chuối' , 'calories' : 85},
+    {'name' : 'cơm' , 'calories' : 130},
+    {'name' : 'trứng luộc' , 'calories' : 75},
+
+  ]
+
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
