971916e350b9947dffe766d70d444ebe848f39b3
Phương Linh <23010562@st.phenikaa-uni.edu.vn>
2025-10-27 22:50:28 +0700

    start

---
+++ calories/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'man_hinh_chinh.dart';
+
+void main() {
+  runApp(const CalorieApp());
+}
+
+class CalorieApp extends StatelessWidget {
+  const CalorieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calorie Counter',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: man_hinh_chinh(), 
+    );
+  }
+}
---
+++ calories/lib/man_hinh_chinh.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class man_hinh_chinh
+    extends StatefulWidget {
+  const man_hinh_chinh({super.key});
+
+  @override
+  State<man_hinh_chinh> createState() =>
+      _man_hinh_chinhState();
+}
+
+class _man_hinh_chinhState
+    extends State<man_hinh_chinh> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
